3a608565cdd6192712fdc0288a0700473c6a7f80
Alvaro Leal <pursuance@gmail.com>
2021-02-15 13:42:55 +0100

    Init

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,82 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { pair, pairWithMatches } from './pairs';
+
+const devs = [
+  'María',
+  'Daniel Fernandez',
+  'Pablo Pareja',
+  'Andrés González',
+  'Javi Abia',
+  'David Inga',
+  'Tomas Eriksson',
+  'Miguel Barrenechea',
+  'Alvaro',
+  'Clément'
+];
+
+const existingPairs = [
+  ['María', 'Daniel Fernandez'],
+  ['Pablo Pareja', 'Andrés González'],
+  ['Javi Abia', 'David Inga'],
+  ['Tomas Eriksson', 'Miguel Barrenechea'],
+  ['Alvaro', 'Clément']
+];
+
+const pairs = pair(devs, existingPairs);
+const pairsWithMatch = pairWithMatches(devs, existingPairs);
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Vizzualiy Pairs</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Vizzuality Pairs
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          This week pairs
+          {pairs.map((dev) => (
+            <div>{dev.join(', ')}</div>
+          ))}
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p className={styles.description}>
+          {pairsWithMatch.map((pair) => (
+            <>
+              <>
+                <div className={styles.description}>
+                  Hi <span className={styles.bold}>{pair.dev1}</span>,
+                </div>
+                <div>Your pair this week is: {pair.dev2},</div>
+                <div>Your common interests are:</div>
+                <div className={styles.code}>
+                  {pair.commonInterests.map((i) => (
+                    <div>{i}</div>
+                  ))}
+                </div>
+              </>
+              <>
+                <div className={styles.description}>
+                  Hi <span className={styles.bold}>{pair.dev2}</span>,
+                </div>
+                <div>Your pair this week is: {pair.dev1},</div>
+                <div>Your common interests are:</div>
+                <div className={styles.code}>
+                  {pair.commonInterests.map((i) => (
+                    <div>{i}</div>
+                  ))}
+                </div>
+              </>
+            </>
+          ))}
+        </p>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
