a4c897b090c532591d03cd4fcdcdcdfb25009d9e
Alvaro Leal <pursuance@gmail.com>
2021-02-15 13:50:05 +0100

    Get logic out of pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { pair, pairWithMatches } from './pairs';
+import { pair, pairWithMatches } from '../components/pairs';
 
 const devs = [
   'María',
